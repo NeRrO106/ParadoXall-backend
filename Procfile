8f69103fb8bdabff041053e6f8d17db13bd25c23
NeRrO106 <andreilimit@yahoo.com>
2024-08-29 16:42:20 +0300

    da

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn backend.wsgi:application --chdir website/src/backend
+web: gunicorn backend.wsgi --log-file -
--- backend/settings.py
+++ backend/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://paradoxall-80370d8dd2e4.herokuapp.com/']
 
 
 # Application definition
