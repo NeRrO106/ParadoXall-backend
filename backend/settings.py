fc5574df3d04626f8e888b090b449b65cc55f4a3
NeRrO106 <andreilimit@yahoo.com>
2024-11-25 16:48:25 +0200

    order email update

--- backend/settings.py
+++ backend/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
 }
 
 # Email
-
+"""
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -98,7 +98,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
