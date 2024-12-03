2741da432d1f2cbe956d79f372b7c0ba1ad19ae3
NeRrO106 <andreilimit@yahoo.com>
2024-12-03 21:00:06 +0200

    add product

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import get_products, create_order, get_product, get_orders, put_order, delete_product, edit_product
+from .views import get_products, create_order, get_product, get_orders, put_order, delete_product, edit_product, create_product
 
 urlpatterns = [
     path('products/', get_products, name='product-list'),
     path('products/<int:id>/', get_product, name='product-detail'),
     path('create-order/', create_order, name='create_order'),
+    path('add-product', create_product, name='create_product'),
     path('orders/', get_orders, name="get_orders"),
     path('orders/<int:id>/complete/', put_order, name='complete_order'),
     path('delete-product/<int:id>/', delete_product, name="delete_product"),
--- api/views.py
+++ api/views.py
@@ -7,9 +7,6 @@ from rest_framework import status
 from .serializers import OrderSerializer, ProductSerializer
 from django.shortcuts import get_object_or_404
 
-from django.core.mail import send_mail
-from django.conf import settings
-
 def get_products(request):
     products = Product.objects.all()
     product_list = list(products.values())
@@ -66,9 +63,6 @@ def create_order(request):
     serializer = OrderSerializer(data=request.data)
     if serializer.is_valid():
         order = serializer.save()
-
-        #send_order_to_restaurant(request.data)
-
         return Response({'message:': 'Order placed succesfully'}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -104,45 +98,10 @@ def edit_product(request, id):
         return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#def send_order_to_restaurant(order_data):
- #   subject='O noua comanda'
-
-  #  delivery_method_display = 'Ridicare' if order_data['delivery_method'] == 'pickup' else 'Livrare'
-  #  delivery_adress_display = 'N/A' if order_data['address'] == 'ridicare' else order_data['address']
-  #  delivery_city_display = 'N/A' if order_data['city'] == 'ridicare' else order_data['city']
-
-   # message=f"""
-    #    O noua comanda a fost plasata:
-     #   - Nume client: {order_data['customer_name']}
-      #  - Telefon: {order_data['phone_number']}
-       # - Metoda de livrare: {delivery_method_display}
-       # - Adresa: {delivery_adress_display}
-       # - Oras: {delivery_city_display}
-       # - Judet: {order_data['region']}
-       # - Metoda de plata: {order_data['payment_methods']}
-       # - Informatii suplimentare: {order_data.get('additional_info', 'N/A')}
-
-#        Produse comandate:    
- #   """
-  #  total_price = 0
-
-   # for item in order_data['order_items']:
-    #    product = Product.objects.get(pk=item['product'])
-    #    item_name = product.name
-    #    item_price = product.price
-    #    item_total = item_price * item['quantity']
-    #    total_price += item_total
-
-    #    message += f"     -Produs: {item_name}, {item['selected_option']}, \nCantitate: {item['quantity']}, Pret: {item_price} lei,\n"   
-
-    #message += f"\nTotal comanda: {total_price} lei"
-
-    #restaurant_email = 'andreilimit66@gmail.com'
-
-    """send_mail(
-        subject,
-        message,
-        settings.EMAIL_HOST_USER,
-        [restaurant_email],
-        fail_silently=False,
-    )"""
+@api_view(['POST'])
+def create_product(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'message:': 'Product added succesfully'}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
